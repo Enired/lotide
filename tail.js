b39440437ad90ced9ae5f8d6db3306124af18ae2
Derine Banis <d.banis.13@gmail.com>
2022-05-30 15:34:05 -0700

    Adding own tail.js and fixing lint errors of previous files.

---
+++ tail.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+//Test Arrays
+const testArray1 = [1,2,3,4,5];
+const testArray2 = ['Luis', 'Angus', 'Ryan', 'Raymond'];
+//Testing for tail functionality.
+console.log(tail(testArray1));
+console.log(tail(testArray2));
+//Testing to ensure original array is not modified.
+console.log(testArray1);
+console.log(testArray2);
+//Testing assertEqual with tail.
+const testCase = function(returnedArray, expectedArray) {
+  for (let i = 0; i < returnedArray.length; i++) {
+    assertEqual(returnedArray[i], expectedArray[i]);
+  }
+};
+//Expected tail of Test Arrays
+const testArray1ExpectedTail = [2,3,4,5];
+const testArray2ExpectedTail = ['Angus', 'Ryan', 'Raymond'];
+testCase(tail(testArray1),testArray1ExpectedTail);
+testCase(tail(testArray2), testArray2ExpectedTail);
+
