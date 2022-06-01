132bba7180a13212592063c9a305600308405183
Derine Banis <d.banis.13@gmail.com>
2022-06-01 13:00:09 -0700

    Adding middle.js to project

---
+++ middle.js
@@ -0,0 +1,71 @@
+//TEST + ASSERTION FUNCTIONS
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✔️Assertion passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`❌Assertion failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+const checkArrayMinLength = function(array){
+  if(array.length < 3){
+    return true;
+  }
+  return false;
+}
+
+
+const checkIfArrayIsEven = function(array){ //Only need to check for even, because if it's not even it's odd.
+  if(array.length % 2 === 0){
+    return true;
+  }
+  return false;
+}
+
+const findIndexOfMiddle = function(array){
+  return Math.floor(array.length / 2);
+}
+
+const findMiddle = function(array){
+  let middle = [];
+  if(checkArrayMinLength(array)){
+    return middle;
+  }
+  if(checkIfArrayIsEven(array)){
+    middle.push(array[findIndexOfMiddle(array) - 1]) 
+    middle.push(array[findIndexOfMiddle(array)])
+    return middle;
+  }
+  middle.push(array[findIndexOfMiddle(array)]);
+  return middle;
+}
+
+//Test Code
+const testCaseNum1 = findMiddle([0,1,2,3,4,5])
+const testCaseNum2 = findMiddle([0,1,2,3,4,5,6])
+
+console.log(testCaseNum1)
+console.log(testCaseNum2)
+const testCaseString1 = findMiddle(['Ryan','Luis','Angus','Raymond']);
+const testCaseString2 = findMiddle(['Ryan','Luis','Angus','Raymond','Tommy'])
+console.log(testCaseString1)
+console.log(testCaseString2)
+
+assertArraysEqual(testCaseNum1, [2,3])
+assertArraysEqual(testCaseNum2, [3])
+
+assertArraysEqual(testCaseString1, ['Luis', 'Angus'])
+assertArraysEqual(testCaseString2, ['Angus'])
+
