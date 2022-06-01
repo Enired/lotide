8b97b638e3d6cd9a56ef9221c4a0f076580b2883
Derine Banis <d.banis.13@gmail.com>
2022-05-31 18:12:12 -0700

    Adding eqArray.js to lotide project

---
+++ eqArray.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(['Angus', 'Luis', 'Ryan'], ['Angus', 'Luis', 'Ryan']), true);
+assertEqual(eqArrays([1, '2', 3], [1, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], []), false);
+assertEqual(eqArrays([1, 2, 3], [1, null, 3]), false);
