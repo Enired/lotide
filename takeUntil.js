5a25ba886e51f4e2fddd34fcb5a0ea53ee10f7f6
Derine Banis <d.banis.13@gmail.com>
2022-06-02 15:44:19 -0700

    Adding takeUntil.js to project.

---
+++ takeUntil.js
@@ -0,0 +1,56 @@
+//ASSERT FUNCTIONS
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length === arrayTwo.length) {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    return console.log(`✔️Assertion passed: ${arrayOne} === ${arrayTwo}`);
+  }
+  return console.log(`❌Assertion failed: ${arrayOne} !== ${arrayTwo}`);
+  
+};
+
+//MAIN CODE
+const takeUntil = function(anArray, callBackFunction) {
+  let slicedArray = [];
+  for (let element of anArray) {
+    if (callBackFunction(element)) {
+      return slicedArray;
+    }
+    slicedArray.push(element);
+  }
+  return slicedArray;
+};
+
+
+//TESTING
+console.log('TEST 1');
+console.log(`-----`);
+const apexLegends = ['Wraith', 'Octane', 'Fuse', 'Horizon', 'Mirage', 'Ash', 'Rampart', 'Pathfinder', 'Bloodhound', 'Crypto'];
+
+const alphaResult = takeUntil(apexLegends, legend => legend === 'Ash');
+const alphaExpected = ['Wraith', 'Octane', 'Fuse', 'Horizon', 'Mirage'];
+assertArraysEqual(alphaResult,alphaExpected);
+console.log(`-----`);
+console.log();
+console.log(`TEST 2`);
+console.log(`-----`);
+const betaResult = takeUntil(apexLegends, legend => legend.length > 7);
+const betaExpected = ['Wraith', 'Octane', 'Fuse', 'Horizon', 'Mirage', 'Ash', 'Rampart'];
+assertArraysEqual(betaResult,betaExpected);
+console.log(`-----`);
+console.log();
+console.log(`TEST 3`);
+console.log(`-----`);
+const charlieResult = takeUntil(apexLegends,legend => legend.includes('B'));
+const charlieExpected = ['Wraith', 'Octane', 'Fuse', 'Horizon', 'Mirage', 'Ash', 'Rampart', 'Pathfinder'];
+assertArraysEqual(charlieResult, charlieExpected);
