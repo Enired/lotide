961339ec61bf37e529e6dd21d749d73c427543c4
Derine Banis <d.banis.13@gmail.com>
2022-06-01 19:04:31 -0700

    Adding eqObjects.js to project. *Forgot to Eslint it vefore push*

--- eqObjects.js
+++ eqObjects.js
@@ -20,35 +20,35 @@ const eqArrays = function(arrayOne, arrayTwo) {
 };
 
 
-const eqObjects = function(objectOne, objectTwo){
-  if(Object.keys(objectOne).length !== Object.keys(objectTwo).length){
+const eqObjects = function(objectOne, objectTwo) {
+  if (Object.keys(objectOne).length !== Object.keys(objectTwo).length) {
     return false;
   }
-  for(let key in objectOne){
-    if(Array.isArray(objectOne[key]) && Array.isArray(objectTwo[key])){
-      return eqArrays(objectOne[key], objectTwo[key])
+  for (let key in objectOne) {
+    if (Array.isArray(objectOne[key]) && Array.isArray(objectTwo[key])) {
+      return eqArrays(objectOne[key], objectTwo[key]);
     }
-    if(objectOne[key] !== objectTwo[key]){
+    if (objectOne[key] !== objectTwo[key]) {
       return false;
     }
   }
-return true;
+  return true;
 
-}
+};
 
 //Testing
-console.log('Assert Test Part 1')
+console.log('Assert Test Part 1');
 const ab = {a: '1', b: '2'};
 const ba = {b: '2', a: '1'};
-const abc = {a: '1', b: '2', c: '3'}
+const abc = {a: '1', b: '2', c: '3'};
 
-assertEqual(eqObjects(ab, ba), true) 
-assertEqual(eqObjects(ab, abc), false)
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
 
-console.log('Assert Test Part 2')
+console.log('Assert Test Part 2');
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 
 assertEqual(eqObjects(cd,dc), true);
-assertEqual(eqObjects(cd,cd2), false)
+assertEqual(eqObjects(cd,cd2), false);
