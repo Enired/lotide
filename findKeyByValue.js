aed0b49e83e41589dd23c9bfdfc8229e07e64076
Derine Banis <d.banis.13@gmail.com>
2022-06-01 18:26:53 -0700

    Adding findKeyByValue.js to project

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+//Assert Function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(givenObj,givenVal) {
+  let foundKey;
+  for (let key in givenObj) {
+    if (givenObj[key] === givenVal) {
+      foundKey = key;
+    }
+  }
+  return foundKey;
+};
+
+//Testing
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), 'comedy');
+
+const allMyFriendsAge = {
+  ryan : 27,
+  angus : 30,
+  luis : 31,
+};
+
+assertEqual(findKeyByValue(allMyFriendsAge, 30), 'angus');
+assertEqual(findKeyByValue(allMyFriendsAge, 27), 'ryan');
