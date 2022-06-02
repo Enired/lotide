b4492c7e8470f5235a376eaf7a3854f54b66d8e9
Derine Banis <d.banis.13@gmail.com>
2022-06-01 19:15:34 -0700

    Adding assertObjectsEqual.js to project.

---
+++ assertObjectsEqual.js
@@ -0,0 +1,28 @@
+const eqObjects = function(objectOne, objectTwo) {
+  if (Object.keys(objectOne).length !== Object.keys(objectTwo).length) {
+    return false;
+  }
+  for (let key in objectOne) {
+    if (Array.isArray(objectOne[key]) && Array.isArray(objectTwo[key])) {
+      return eqArrays(objectOne[key], objectTwo[key]);
+    }
+    if (objectOne[key] !== objectTwo[key]) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+const assertObjectsEqual = function(objectOne, objectTwo){
+  const inspect = require('util').inspect;
+  if (eqObjects(objectOne, objectTwo)) {
+    console.log(`✔️Assertion passed: ${inspect(objectOne)} === ${inspect(objectTwo)}`);
+  } else {
+    console.log(`❌Assertion failed: ${inspect(objectOne)} !== ${inspect(objectTwo)}`);
+  
+}
+}
+
+assertObjectsEqual({hello:1,goodbye:2},{goodbye:2, hello:1})
+assertObjectsEqual({hello:1,goodbye:2}, {hello:1,goodbye:2, sayonara: 3})
