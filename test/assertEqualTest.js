ea7b669f6ca082640948944889318cfe5bd26a04
Derine Banis <d.banis.13@gmail.com>
2022-06-06 12:48:34 -0700

    Created test file for assertEqual.js in error driven format.

---
+++ test/assertEqualTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require('../assertEqual')
+
+
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(true, false);
+assertEqual('Netflix', 'Netflix');
+assertEqual('Disney', "Disney");
+assertEqual('light', 'LIGHT');
+assertEqual('Dark', 'dark');
+assertEqual(4.0, 4);
+assertEqual(6,9);
