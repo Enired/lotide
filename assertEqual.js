86b9172690941f9a0e6257cc3cc3f922eb56a732
Derine Banis <d.banis.13@gmail.com>
2022-06-06 12:50:33 -0700

    Removed test cases. Exported code for use by other files.

--- assertEqual.js
+++ assertEqual.js
@@ -6,14 +6,4 @@ const assertEqual = function(actual, expected) {
   
 };
 
-// assertEqual('Lighthouse Labs', 'Bootcamp');
-// assertEqual(1, 1);
-// assertEqual(true, false);
-// assertEqual('Netflix', 'Netflix');
-// assertEqual('Disney', "Disney");
-// assertEqual('light', 'LIGHT');
-// assertEqual('Dark', 'dark');
-// assertEqual(4.0, 4);
-// assertEqual(6,9);
-
 module.exports = assertEqual;
