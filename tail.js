2f59379542f6fd229e10e10e320d24eda08c0a65
Derine Banis <d.banis.13@gmail.com>
2022-06-06 15:31:30 -0700

    Removed old , commented out test code.

--- tail.js
+++ tail.js
@@ -1,17 +1,3 @@
-////////////////////////
-// Used for Old Tests //
-////////////////////////
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     return console.log(`✔️Assertion passed: ${actual} === ${expected}`);
-//   }
-//   return console.log(`❌Assertion failed: ${actual} !== ${expected}`);
-  
-// };
-
-
-
-
 const tail = function(array) {
   let newArray = [];
   if(array && Array.isArray(array)){
@@ -23,34 +9,3 @@ const tail = function(array) {
 };
 
 module.exports = tail;
-
-////////////////////
-// Old Test Cases //
-////////////////////
-
-
-// //Test Arrays
-// const testArray1 = [1,2,3,4,5];
-// const testArray2 = ['Luis', 'Angus', 'Ryan', 'Raymond'];
-// const testArray3 = ['Lighthouse Labs'];
-// const testArray4 = [];
-// //Testing for tail functionality.
-// console.log(tail(testArray1));
-// console.log(tail(testArray2));
-// console.log(tail(testArray3));
-// console.log(tail(testArray4));
-// //Testing to ensure original array is not modified.
-// console.log(testArray1);
-// console.log(testArray2);
-// //Testing assertEqual with tail.
-// const testCase = function(returnedArray, expectedArray) {
-//   for (let i = 0; i < returnedArray.length; i++) {
-//     assertEqual(returnedArray[i], expectedArray[i]);
-//   }
-// };
-// //Expected tail of Test Arrays
-// const testArray1ExpectedTail = [2,3,4,5];
-// const testArray2ExpectedTail = ['Angus', 'Ryan', 'Raymond'];
-// testCase(tail(testArray1),testArray1ExpectedTail);
-// testCase(tail(testArray2), testArray2ExpectedTail);
-
