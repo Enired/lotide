8fd6b3f5c2fea5570bd736280a699c06a2d30c09
Derine Banis <d.banis.13@gmail.com>
2022-06-06 14:48:34 -0700

    Changed assertion functions

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,7 @@
-const mocha = require('mocha');
-const assert = mocha.assert;
+const chai = require('chai');
+const assert = chai.assert;
 
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
 //////////////////////////
 // Main Test Code Block //
@@ -13,40 +12,40 @@ describe('Tests for head.js. Returns the value in position 0 of an array.', func
     testArray = [5,6,7];
     result = head(testArray);
     expected = 5;
-    assertEqual(result,expected);
+    assert.strictEqual(result, expected)
   })
 
   it('should return the first value in an array of strings', function(){
     testArray = ['Mirage', 'Bloodhound', 'Octane'];
     result = head(testArray);
     expected = 'Mirage'
-    assertEqual(result,expected);
+    assert.strictEqual(result,expected);
   })
 
   it('should return undefined when given an empty array.', function(){
     testArray = [];
     result = head(testArray);
     expected = undefined;
-    assertEqual(result,expected);
+    assert.strictEqual(result,expected);
   })
 
   it('should return the only value in an array with 1 item in it.', function(){
     testArray = ['Pathfinder']
     result = head(testArray);
     expected = 'Pathfinder'
-    assertEqual(result,expected);
+    assert.strictEqual(result,expected);
   })
 
   it('should return undefined when not passed a parameter.', function(){
     let testArray;
     const result = head(testArray);
     expected = undefined;
-    assertEqual(result,expected);
+    assert.strictEqual(result,expected);
   })
 
   it('should return undefined when a parameter is not passed in.', function(){
     const result = head();
     expected = undefined;
-    assertEqual(result,expected);
+    assert.strictEqual(result,expected);
   })
 })
