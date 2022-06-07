adaac3c7568135dfcac0293e62cc54cdf7703d47
Derine Banis <d.banis.13@gmail.com>
2022-06-06 20:54:53 -0700

    Removed old, commented out code

--- flatten.js
+++ flatten.js
@@ -1,34 +1,3 @@
-// const eqArrays = function(arrayOne, arrayTwo) {
-//   if (arrayOne.length === arrayTwo.length) {
-//     for (let i = 0; i < arrayOne.length; i++) {
-//       if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
-//         if (!eqArrays(arrayOne[i],arrayTwo[i])) {
-//           return false;
-//         }
-//       } else {
-//         if (arrayOne[i] !== arrayTwo[i]) {
-//           return false;
-//         }
-//       }
-//     }
-//     return true;
-//   }
-//   return false;
-// };
-
-
-// const assertArraysEqual = function(arrayOne, arrayTwo) {
-//   if (eqArrays(arrayOne, arrayTwo)) {
-//     return console.log(`✔️Assertion passed: ${arrayOne} === ${arrayTwo}`);
-//   }
-//   return console.log(`❌Assertion failed: ${arrayOne} !== ${arrayTwo}`);
-  
-// };
-
-
-// const eqArrays = require('./eqArrays');
-
-
 const flatten = function(array) {
   let flattenedArray = [];
   if(array && Array.isArray(array)){
