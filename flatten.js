d10be213a696f3d445f5fc3697829117851eca42
Derine Banis <d.banis.13@gmail.com>
2022-06-06 19:20:37 -0700

    Implemented logic to account for non-array parameters and for no parameter passed

--- flatten.js
+++ flatten.js
@@ -31,13 +31,16 @@
 
 const flatten = function(array) {
   let flattenedArray = [];
-  for (let element of array) {
-    Array.isArray(element) ? flattenedArray = flattenedArray.concat(element) : flattenedArray.push(element);
-  }
-  //Recursion to flatten deeper arrays.
-  for (let element of flattenedArray) {
-    if (Array.isArray(element)) {
-      flattenedArray = flatten(flattenedArray);
+  if(array && Array.isArray(array)){
+
+    for (let element of array) {
+      Array.isArray(element) ? flattenedArray = flattenedArray.concat(element) : flattenedArray.push(element);
+    }
+    //Recursion to flatten deeper arrays.
+    for (let element of flattenedArray) {
+      if (Array.isArray(element)) {
+        flattenedArray = flatten(flattenedArray);
+      }
     }
   }
 
@@ -46,13 +49,3 @@ const flatten = function(array) {
 
 module.exports = flatten;
 
-//Test Cases
-// const test1 = [1, 2, [3, 4], 5, [6]];
-// const test2 = [1, 2, [3, 4, [5, [6,[7,8]]]]];
-// const expect1 = [1,2,3,4,5,6];
-// const expect2 = [1,2,3,4,5,6,7,8];
-// console.log(flatten(test1));
-// console.log(flatten(test2));
-
-// assertArraysEqual(flatten(test1), expect1);
-// assertArraysEqual(flatten(test2), expect2);
