def2e9c7b021e7f01daab6c87f66d0c260226434
Derine Banis <d.banis.13@gmail.com>
2022-06-06 17:34:30 -0700

    Did not declare variables with const keyword. Fixed

--- test/headTest.js
+++ test/headTest.js
@@ -7,45 +7,45 @@ const head = require('../head');
 // Main Test Code Block //
 //////////////////////////
 
-describe('Tests for head.js. Returns the value in position 0 of an array.', function(){
-  it('should return the first value in an array of numbers.', function(){
-    testArray = [5,6,7];
-    result = head(testArray);
-    expected = 5;
-    assert.strictEqual(result, expected)
-  })
+describe('Tests for head.js. Returns the value in position 0 of an array.', function() {
+  it('should return the first value in an array of numbers.', function() {
+    const testArray = [5,6,7];
+    const result = head(testArray);
+    const expected = 5;
+    assert.strictEqual(result, expected);
+  });
 
-  it('should return the first value in an array of strings', function(){
-    testArray = ['Mirage', 'Bloodhound', 'Octane'];
-    result = head(testArray);
-    expected = 'Mirage'
+  it('should return the first value in an array of strings', function() {
+    const testArray = ['Mirage', 'Bloodhound', 'Octane'];
+    const result = head(testArray);
+    const expected = 'Mirage';
     assert.strictEqual(result,expected);
-  })
+  });
 
-  it('should return undefined when given an empty array.', function(){
-    testArray = [];
-    result = head(testArray);
-    expected = undefined;
+  it('should return undefined when given an empty array.', function() {
+    const testArray = [];
+    const result = head(testArray);
+    const expected = undefined;
     assert.strictEqual(result,expected);
-  })
+  });
 
-  it('should return the only value in an array with 1 item in it.', function(){
-    testArray = ['Pathfinder']
-    result = head(testArray);
-    expected = 'Pathfinder'
+  it('should return the only value in an array with 1 item in it.', function() {
+    const testArray = ['Pathfinder'];
+    const result = head(testArray);
+    const expected = 'Pathfinder';
     assert.strictEqual(result,expected);
-  })
+  });
 
-  it('should return undefined when not passed a parameter.', function(){
+  it('should return undefined when not passed a parameter.', function() {
     let testArray;
     const result = head(testArray);
-    expected = undefined;
+    const expected = undefined;
     assert.strictEqual(result,expected);
-  })
+  });
 
-  it('should return undefined when a parameter is not passed in.', function(){
+  it('should return undefined when a parameter is not passed in.', function() {
     const result = head();
-    expected = undefined;
+    const expected = undefined;
     assert.strictEqual(result,expected);
-  })
-})
+  });
+});
