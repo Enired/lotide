9ae7dc8c0010ebd5f9910b15541b5db726410da6
Derine Banis <d.banis.13@gmail.com>
2022-06-06 17:36:46 -0700

    Commented out test code

--- eqObjects.js
+++ eqObjects.js
@@ -7,23 +7,23 @@
   
 // };
 
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length === arrayTwo.length) {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
-        if (!eqArrays(arrayOne[i],arrayTwo[i])) {
-          return false;
-        }
-      } else {
-        if (arrayOne[i] !== arrayTwo[i]) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-  return false;
-};
+// const eqArrays = function(arrayOne, arrayTwo) {
+//   if (arrayOne.length === arrayTwo.length) {
+//     for (let i = 0; i < arrayOne.length; i++) {
+//       if (Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
+//         if (!eqArrays(arrayOne[i],arrayTwo[i])) {
+//           return false;
+//         }
+//       } else {
+//         if (arrayOne[i] !== arrayTwo[i]) {
+//           return false;
+//         }
+//       }
+//     }
+//     return true;
+//   }
+//   return false;
+// };
 
 
 const eqArrays = require('./eqArrays');
@@ -60,86 +60,86 @@ const eqObjects = function(objectOne, objectTwo) {
 
 };
 
-//Testing
-// console.log('Assert Test Part 1');
-// const ab = {a: '1', b: '2'};
-// const ba = {b: '2', a: '1'};
-// const abc = {a: '1', b: '2', c: '3'};
+// //Testing
+// // console.log('Assert Test Part 1');
+// // const ab = {a: '1', b: '2'};
+// // const ba = {b: '2', a: '1'};
+// // const abc = {a: '1', b: '2', c: '3'};
 
-// assertEqual(eqObjects(ab, ba), true);
-// assertEqual(eqObjects(ab, abc), false);
+// // assertEqual(eqObjects(ab, ba), true);
+// // assertEqual(eqObjects(ab, abc), false);
 
-// console.log('Assert Test Part 2');
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// const cd2 = { c: "1", d: ["2", 3, 4] };
+// // console.log('Assert Test Part 2');
+// // const cd = { c: "1", d: ["2", 3] };
+// // const dc = { d: ["2", 3], c: "1" };
+// // const cd2 = { c: "1", d: ["2", 3, 4] };
 
-// assertEqual(eqObjects(cd,dc), true);
-// assertEqual(eqObjects(cd,cd2), false);
+// // assertEqual(eqObjects(cd,dc), true);
+// // assertEqual(eqObjects(cd,cd2), false);
 
-//Further Testing for recursion implementation
-const apexLegendsAlpha = {
-  Wraith: {
-    gender: 'female',
-    hair: false
-  },
-  Bloodhound : {
-    gender: 'female',
-    hair: true
-  }
-};
-const apexLegendsBeta = {
-  Wraith: {
-    gender: 'female',
-    hair: false
-  },
-  Bloodhound : {
-    gender: 'female',
-    hair: true
-  }
-};
+// //Further Testing for recursion implementation
+// const apexLegendsAlpha = {
+//   Wraith: {
+//     gender: 'female',
+//     hair: false
+//   },
+//   Bloodhound : {
+//     gender: 'female',
+//     hair: true
+//   }
+// };
+// const apexLegendsBeta = {
+//   Wraith: {
+//     gender: 'female',
+//     hair: false
+//   },
+//   Bloodhound : {
+//     gender: 'female',
+//     hair: true
+//   }
+// };
 
-const apexLegendsCharlie = {
-  Wraith: {
-    gender: 'female',
-    hair: false
-  },
-  Bloodhound : {
-    gender: 'female',
-    hair: {
-      helmet: true,
-      hat: true
-    }
-  }
-};
-const apexLegendsDelta = {
-  Wraith: {
-    gender: 'female',
-    hair: false
-  },
-  Bloodhound : {
-    gender: 'female',
-    hair: {
-      helmet: true,
-      hat: true
-    }
-  }
-};
-const apexLegendsEcho = {
-  Wraith: {
-    gender: 'female',
-    hair: false
-  },
-  Bloodhound : {
-    gender: 'female',
-    hair: {
-      helmet: true,
-      hat: {
-        metal:true
-      }
-    }
-  }
-};
+// const apexLegendsCharlie = {
+//   Wraith: {
+//     gender: 'female',
+//     hair: false
+//   },
+//   Bloodhound : {
+//     gender: 'female',
+//     hair: {
+//       helmet: true,
+//       hat: true
+//     }
+//   }
+// };
+// const apexLegendsDelta = {
+//   Wraith: {
+//     gender: 'female',
+//     hair: false
+//   },
+//   Bloodhound : {
+//     gender: 'female',
+//     hair: {
+//       helmet: true,
+//       hat: true
+//     }
+//   }
+// };
+// const apexLegendsEcho = {
+//   Wraith: {
+//     gender: 'female',
+//     hair: false
+//   },
+//   Bloodhound : {
+//     gender: 'female',
+//     hair: {
+//       helmet: true,
+//       hat: {
+//         metal:true
+//       }
+//     }
+//   }
+// };
 
 // assertEqual(eqObjects(apexLegendsAlpha, apexLegendsBeta), true);
 // assertEqual(eqObjects(apexLegendsAlpha, apexLegendsCharlie), false);
